9071cbdd4cb9964d90afd2e12efdb6af5ec280cf
jacques <holyseven@hotmail.com>
2018-02-27 17:48:07 +0100

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # TransferLearningClassification
+
+Steps of reproducing the experiments:
+
+1. Download _resnet_v1_101.ckpt_ from tensorflow slim with the script `./z_pretrained_weights/download_resnet_v1_101.sh`.
+
+2. (optional) Verify the pre-trained weights, using `./network_verification/resnet_verification.py`.
+ImageNet validation set is needed.
+
+3. a. Download bin version data files and change the path of databases in `./database/*.py`.
+
+   b. Create databases and Re-implement the pre-processing of the inputs.
+
+4. run the script `./run_classification/train.sh`
+
+L2-SP should have around 85% classification accuracy while L2 nearly 80%.
+Tested with Tensorflow 1.4.
+
---
+++ run_classification/train.sh
@@ -0,0 +1,7 @@
+n=0
+
+CUDA_VISIBLE_DEVICES=$n python run_exp.py --lrn_rate 0.01 --weight_decay_mode 0 --weight_decay_rate 0.0001 --weight_decay_rate2 0.001 --momentum 0.9 --batch_size 64 --train_max_iter 9000 --snapshot 9000 --fine_tune_filename '../z_pretrained_weights/resnet_v1_101.ckpt' --optimizer 'mom' --color_switch 1 --step_size 0.1 --lr_step 5000 --resnet 'resnet_v1_101' --database 'dogs120' --test_batch_size 100 --log_dir 0
+
+CUDA_VISIBLE_DEVICES=$n python run_exp.py --lrn_rate 0.01 --weight_decay_mode 1 --weight_decay_rate 0.1 --weight_decay_rate2 0.01 --momentum 0.9 --batch_size 64 --train_max_iter 9000 --snapshot 9000 --fine_tune_filename '../z_pretrained_weights/resnet_v1_101.ckpt' --optimizer 'mom' --color_switch 1 --step_size 0.1 --lr_step 5000 --resnet 'resnet_v1_101' --database 'dogs120' --test_batch_size 100 --log_dir 0
+
+# CUDA_VISIBLE_DEVICES=$n python run_exp.py --lrn_rate 0.005 --weight_decay_mode 1 --weight_decay_rate 0.1 --weight_decay_rate2 0.01 --momentum 0.9 --batch_size 64 --train_max_iter 9000 --snapshot 9000 --server $s --fine_tune_filename '../z_pretrained_weights/resnet_v1_101.ckpt' --optimizer 'mom' --color_switch 1 --step_size 0.1 --lr_step 5000 --resnet 'resnet_v1_101' --database 'dogs120' --test_batch_size 100 --log_dir 0
