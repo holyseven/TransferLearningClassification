d6e4db65f27e548d5bca20787d7f075acbef7718
jacques <holyseven@hotmail.com>
2018-04-25 22:13:13 +0200

    minor change

--- database/dataset_reader.py
+++ database/dataset_reader.py
@@ -169,6 +169,7 @@ def build_input(batch_size, mode, dataset='dogs120', blur=True, color_switch=Fal
             num_files = int(examples_per_class // 5)
             data_files = sorted(data_files)[0:num_files]
         print(data_files)
+        assert len(data_files) > 0, 'No database is found.'
 
         file_queue = tf.train.string_input_producer(data_files, shuffle=(mode == 'train'))
 
